84d2fabcb017a09c0dd7ed775d9905c647eb4f53
влада ражина <razhina.vv@phystech.edu>
2025-09-30 18:44:31 +0300

    8-битный R2R ЦАП в виде класса

---
+++ get-dac/r2r-dac.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+    def set_number(self, number):
+        num = [int(element) for element in bin(number)[2:].zfill(8)]
+        print(num)
+        sleep_time = 0.02
+        for i in range(len(self.gpio_bits)):
+            if num[i] == 1:
+                GPIO.output(self.gpio_bits[i], 1)
+                time.sleep(sleep_time)
+            else:
+                GPIO.output(self.gpio_bits[i], 0)
+                time.sleep(sleep_time)
+    def set_voltage(self, voltage):
+        number = int(voltage / self.dynamic_range * 255)
+        dac.set_number(number)
+if __name__ == "__main__":
+    try:
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+
+        while(True):
+            try:
+                voltage = float(input("Введите напрядение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте еще раз\n")
+    finally:
+        dac.deinit()
